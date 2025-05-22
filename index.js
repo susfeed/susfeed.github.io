602a1ef3439e191b0664eefff8277c97be1318db
HollabaughC <susfeedllc@gmail.com>
2025-05-21 23:19:42 -0400

    Cookies

---
+++ index.js
@@ -0,0 +1,30 @@
+async function loadAndDisplayContent(jsonPath, containerId, baseDir) {
+      try {
+        const response = await fetch(jsonPath);
+        const data = await response.json();
+
+        data.sort((a, b) => parseInt(b.folder) - parseInt(a.folder));
+
+        const topThree = data.slice(0, 3);
+        const container = document.getElementById(containerId);
+
+        topThree.forEach(item => {
+          const title = item.file.replace(/_/g, ' ').replace(/\.html$/, '');
+          const card = document.createElement('a');
+          card.href = `${baseDir}/${item.folder}/${item.file}`;
+          card.className = 'article-card';
+          card.innerHTML = `
+            <div class="card-image" style="background-image: url('${baseDir}/${item.folder}/cover.webp');"></div>
+            <div class="card-content">
+              <h3 class="card-title">${title}</h3>
+            </div>
+          `;
+          container.appendChild(card);
+        });
+      } catch (error) {
+        console.error(`Failed to load ${jsonPath}:`, error);
+      }
+    }
+
+    loadAndDisplayContent('articles/index.json', 'new-articles', 'articles');
+    loadAndDisplayContent('quizzes/index.json', 'new-quizzes', 'quizzes');
